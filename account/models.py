0cfb5846166fa9b9d8ac437e8439173ab1ef0af2
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-19 16:26:45 +0100

    "Building a student report django project"

--- account/models.py
+++ account/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import User
 from django.db import models
+
+from core.models import User, Department
+
 
 class Student(models.Model):
     class Level(models.TextChoices):
@@ -22,3 +24,26 @@ class Student(models.Model):
     matric_number = models.CharField(max_length=20, unique=True, default="")
     enrolled_at = models.DateField(null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.matric_number
+    
+
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    designation = models.CharField(max_length=55, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f" {self.user} - {self.designation}"
+
+    class Meta:
+        ordering = ['designation']
+
