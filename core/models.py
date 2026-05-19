7d18fcabc28787ac6636a1cc6c62f268d41ddb4f
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-19 16:13:46 +0100

    "Building a student report django project"

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+
+
+class User(AbstractBaseUser):
+    class Role(models.TextChoices):
+        ADMIN = 'admin', 'Admin'
+        USER = 'user', 'User'
+        STUDENT = 'student', 'Student'
+
+    first_name = models.CharField(max_length=250, blank=False, null=False)
+    last_name = models.CharField(max_length=250, blank=False, null=False)
+    email = models.EmailField(max_length=250, unique=True, blank=False, null=False)
+    username = models.CharField(max_length=250, unique=True, blank=False, null=False)
+    password = models.CharField(max_length=250, blank=False, null=False)
+    role = models.CharField(max_length=10, choices=Role, default=Role.STUDENT)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=250, blank=False, null=False)
+    code = models.CharField(max_length=10, unique=True, blank=False, null=False)
+    description = models.CharField(max_length=250, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.name} {self.code}"
+
