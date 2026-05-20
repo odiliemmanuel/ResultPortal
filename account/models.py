2d79b34a2018145607af346474b3544b2d057f23
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-20 11:20:44 +0100

    "Building a student report django project"

--- account/models.py
+++ account/models.py
@@ -1,37 +1,59 @@
 from django.db import models
-
 from core.models import User, Department
+from util import generate_matric_number
+from core.constants import LEVEL_CHOICES, ROLE_STUDENT
 
 
 class Student(models.Model):
-    class Level(models.TextChoices):
-        LEVEL100 = "Level 100"
-        LEVEL200 = "Level 200"
-        Level300 = "Level 300"
-        Level400 = "Level 400"
-        Level500 = "Level 500"
+    STUDENT_STATUS_CHOICES = [
+        ("active", "Active"),
+        ("suspended", "Suspended"),
+        ("graduated", "Graduated"),
+        ("withdrawn", "Withdrawn"),
+    ]
+
+    DESIGNATION_CHOICES = [
+        ("lecturer_i", "Lecture I"),
+        ("lecturer_ii", "Lecturer II"),
+        ("sr_lecturer", "Senior Lecturer"),
+        ("professor", "Professor"),
+        ("hod", "Head of Department"),
+    ]
 
 
-    class Status(models.TextChoices):
-        ACTIVE = "Active"
-        INACTIVE = "Inactive"
-        SUSPENDED = "Suspended"
-        WITHDRAWN = "Withdrawn"
-
-
-    level = models.CharField(max_length=20, choices=Level, default=Level.LEVEL100)
-    status = models.CharField(max_length=20, choices=Status, default=Status.ACTIVE)
-    matric_number = models.CharField(max_length=20, unique=True, default="")
-    enrolled_at = models.DateField(null=True, blank=True)
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
-    department = models.ForeignKey(Department, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="student_profile", limit_choices_to={"role": ROLE_STUDENT},)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, related_name="student")
+    matric_number = models.IntegerField(max_length=20, unique=True, default=generate_matric_number)
+    level = models.CharField(max_length=3, choices=LEVEL_CHOICES)
+    status = models.CharField(max_length=20, choices=STUDENT_STATUS_CHOICES, default="active")
+    entry_year = models.PositiveIntegerField()
+    enrolled_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
+    class Meta:
+        db_table = "student account"
+        ordering = ["matric_number"]
+
+
     def __str__(self):
-        return self.matric_number
-    
+        return f"{self.user.username} - {self.user.get_full_name()}"
+
+
+    @property
+    def full_name(self):
+        return self.user.get_full_name()
+
+    @property
+    def email(self):
+        return self.user.email
+
+    @property
+    def is_active(self):
+        return self.status
+
+
+
 
 class Staff(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
@@ -42,8 +64,8 @@ class Staff(models.Model):
 
 
     def __str__(self):
-        return f" {self.user} - {self.designation}"
+        return f"{self.user.username} - {self.user.get_full_name()}"
+
 
     class Meta:
-        ordering = ['designation']
-
+        ordering = ["designation"]
---
+++ account/util.py
@@ -0,0 +1,5 @@
+from random import random
+
+
+def generate_matric_number():
+    return random.randint(1, 9)
