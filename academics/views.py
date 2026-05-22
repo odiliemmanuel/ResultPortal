09ca4cf7092fc9af7dd40a559764ed994bc59bcc
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-22 13:19:22 +0100

    "Building a student report django project"

--- academics/views.py
+++ academics/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from loguru import logger
+
+from core.models import Department
+from academics.models import Course
+from academics.serializers import CourseSerializer
+
+
+@api_view(['POST'])
+def create_course(request):
+    try:
+        serializer = CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        department_id = serializer.validated_data['department_id']
+
+        try:
+            department = Department.objects.get(id=department_id)
+        except Department.DoesNotExist:
+            logger.error(f"Department with id {department_id} does not exist")
+            return Response({"message": "department does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+        code = serializer.validated_data['code']
+
+        if Course.objects.filter(code=code).exists():
+            logger.error(f"Course with code {code} already exists")
+            return Response({"message": "Course with code already exists"}, status=status.HTTP_409_CONFLICT)
+
+        course = Course.objects.create(
+            department=department,
+            code=serializer.validated_data['code'],
+            title = serializer.validated_data['title'],
+            credit_units = serializer.validated_data['credit_units'],
+            level = serializer.validated_data['level'],
+            semester = serializer.validated_data['semester'],
+            description = serializer.validated_data['description']
+        )
+
+        logger.info(f"Course with code {course.code} created")
+        return Response({
+            "message": "Course created successfully",
+            "data": {
+                "id": course.id,
+                "code": course.code,
+                "title": course.title,
+            }
+        }, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        logger.error(f"Error creating course: {str(e)}")
+        return Response({"message": "Error creating course"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- core/views.py
+++ core/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import status
-from loguru import logger
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from loguru import logger
 from .models import Department
 from .serializers import DepartmentSerializer
-# Create your views here.
+
 
 @api_view(['POST'])
 def create_department(request):
@@ -14,13 +13,57 @@ def create_department(request):
         serializer = DepartmentSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         name = serializer.validated_data['name']
-        logger.info(f"data validated for department {name}")
+        code = serializer.validated_data['code']
+        logger.info(f"data validated for department: {name}")
 
+        if Department.objects.filter(code=code).exists():
+            logger.error(f"department with {code} already exists")
+            return Response({"message": "department with code already exists"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
         Department.objects.create(**serializer.validated_data)
-        logger.info(f"created department {name}")
+        logger.info(f"department {name} created")
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        logger.error(f"Error creating department: {str(e)}")
+        return Response({"message": "Error creating department"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
+@api_view(['GET'])
+def get_department(request, code):
+    department = get_object_or_404(Department, code=code, is_active=True)
+    serializer = DepartmentSerializer(department)
+    logger.info(f"department {code} retrieved")
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+@api_view(['PUT', 'PATCH'])
+def update_department(request, code):
+    department = get_object_or_404(Department, code=code)
+    try:
+        serializer = DepartmentSerializer(department, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        logger.info(f"department {department.name} updated")
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    except ValueError as e:
+        logger.error(f"Error validation department: {str(e)}")
+        return Response({"message": "Error validation department"}, status=status.HTTP_400_BAD_REQUEST)
 
     except Exception as e:
-        logger.error(f"Error creating department {str(e)}")
+        logger.error(f"Error updating department: {str(e)} ")
+        return Response({"message": "Error updating department"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def delete_department(request, code):
+    department = get_object_or_404(Department, code=code)
+    department.is_active = False
+    department.save()
+    logger.info(f"department {code} deleted")
+    return Response(status=status.HTTP_204_NO_CONTENT)
