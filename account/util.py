d7ac4835c98feba9da5ba4ab0a1ee1af07ed1933
odiliemmanuel <odiliejeh09@gmail.com>
2026-06-03 16:15:02 +0100

    "Building a student report django project"

--- account/util.py
+++ account/util.py
@@ -1,5 +1,9 @@
-import random
+import datetime
+from random import random
 
 
 def generate_matric_number():
-    return "FSH" + str(random.randint(1, 999))
+    year = datetime.now().year
+    random_digits = random.randint(1000, 9999)
+
+    return f"STU{year}{random_digits}"
