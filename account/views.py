3098282683b3e51ed53dbafdef79f4b11aaefaf8
odiliemmanuel <odiliejeh09@gmail.com>
2026-06-03 17:21:20 +0100

    "Building a student report django project"

--- account/views.py
+++ account/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import StudentEnrollmentSerializer
+from .models import Student
+from core.models import User
+from django.db import transaction
 
-# Create your views here.
+
+class StudentEnrollment(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = StudentEnrollmentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        with transaction.atomic():
+
+            user = User.objects.create(
+                    email=serializer.validated_data['email'],
+                    username=serializer.validated_data['username'],
+                    first_name=serializer.validated_data['first_name'],
+                    last_name=serializer.validated_data['last_name'],
+                    password=serializer.validated_data['password']
+            )
+
+            student = Student.objects.create(
+                user=user,
+                department=serializer.validated_data['department'],
+                entry_year=serializer.validated_data['entry_year'],
+            )
+
+            user.save()
+            student.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+        # "email": ,
+        # "username": ,
+        # "first_name": ,
+        # "last_name": ,
+        # "password": ,
