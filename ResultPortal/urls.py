b92cb8844eec68a1a17a9a76884fb9072f498b04
odiliemmanuel <odiliejeh09@gmail.com>
2026-06-03 17:21:17 +0100

    "Building a student report django project"

--- ResultPortal/urls.py
+++ ResultPortal/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('api/academic-session/', include('academics.urls'))
+    path('api/academic-session/', include('academics.urls')),
+    path('api/account-session/', include('account.urls')),
 ]
