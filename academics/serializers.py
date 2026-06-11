eccfb5c27275ff2ab7a30aae632bff78798cf29c
odiliemmanuel <odiliejeh09@gmail.com>
2026-06-11 10:47:09 +0100

    "Building a student report django project"

--- academics/serializers.py
+++ academics/serializers.py
@@ -1,6 +1,6 @@
-from django.contrib.sessions.models import Session
 from rest_framework import serializers
-from .models import Course, AcademicSession
+from account.models import Student
+from .models import Course, AcademicSession, CourseRegistration
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -13,7 +13,68 @@ class CourseSerializer(serializers.ModelSerializer):
         return Course.objects.create(department_id=department_id, **validated_data)
 
 
-class AcademicsSessionSerializer(serializers.ModelSerializer):
+class AcademicSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = AcademicSession
-        fields = ['name', 'year', 'is_current', 'start_date', 'end_date']
+        fields = ['name', 'year', 'semester', 'is_current', 'start_date', 'end_date']
+
+
+class ReadAcademicSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AcademicSession
+        fields = ['name', 'year', 'start_date']
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields =['department', 'matric_number', 'level', 'status', 'entry_year']
+
+
+class CourseRegistrationSerializer(serializers.ModelSerializer):
+    course_title = serializers.CharField(source='course.title', read_only=True)
+    student = StudentSerializer(read_only=True)
+    session_semester = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CourseRegistration
+        fields = ['id', 'course', 'course_title', 'session', 'student', 'session_semester', 'register_at']
+
+        read_only_fields = ['id', 'register_at', 'session_semester', 'course_title']
+
+    def get_session_semester(self, obj):
+        return obj.session.get_semester_display()
+
+    def validate(self, attrs):
+        course = attrs.get('course')
+        session = attrs.get('session')
+
+        if course and session:
+            if course.semester != session.semester:
+                raise serializers.ValidationError(
+                    {
+                        "course": (
+                            f"'{course.course_code}' is a {course.get_semester_display()} "
+                            f"course but the selected session is "
+                            f"{session.get_semester_display()}."
+                        )
+                    }
+                )
+
+        student = self.context.get('student')
+        if student and course and session:
+            if CourseRegistration.objects.filter(
+                student=student,
+                course=course,
+                session=session
+            ).exists():
+                raise serializers.ValidationError(
+                    {
+                        "non_field_errors": (
+                            f"You are already registered for "
+                            f"'{course.course_code}' in this session. "
+                        )
+                    }
+                )
+
+        return attrs
--- results/models.py
+++ results/models.py
@@ -1,16 +1,21 @@
 from django.db import models
-
+from academics.models import CourseRegistration
 from account.models import Staff
 
 
-class Result(models.Model):
-    score = models.IntegerField(default=0)
-    grade = models.CharField(max_length=1, blank=False, null=False)
-    grade_point = models.IntegerField(default=0, blank=False, null=False)
+class Results(models.Model):
+    registration = models.OneToOneField(CourseRegistration, on_delete=models.CASCADE, related_name="result")
+    score = models.DecimalField(max_digits=4, decimal_places=2, blank=False, null=False)
+    grade = models.CharField(max_length=2, blank=False, null=False)
+    grade_point = models.DecimalField(max_digits=3, decimal_places=1, blank=False, null=False)
     is_published = models.BooleanField(default=False)
     uploaded_by = models.ForeignKey(Staff, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.grade} - {self.grade_point} - {self.uploaded_by}"
+        return (
+            f"{self.registration.student.matric_number} | "
+            f"{self.registration.course.course_code} | "
+            f"{self.score} ({self.grade}"
+        )
